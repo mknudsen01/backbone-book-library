8382e3008f1da11277bf1ef121ce39c1df980cba
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-21 12:51:27 -0700

    Add form functionality
      * on click for 'add', view:
        * goes through each input element and saves it to an object if
          if has a value
        * adds that object to the library collection after iterating

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -3,6 +3,10 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
   el: '#books',
 
+  events: {
+    'click #add':'addBook'
+  },
+
   initialize: function( initialBooks ){
     this.collection = new app.Library( initialBooks );
     this.render();
@@ -19,5 +23,18 @@ app.LibraryView = Backbone.View.extend({
       model: item
     });
     this.$el.append( bookView.render().el );
+  },
+
+  addBook: function ( e ) {
+    e.preventDefault();
+    var formData = {};
+
+    $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
+      if( $( el ).val() !== '' ){
+        formData[ el.id ] = $( el ).val();
+      }
+    });
+
+    this.collection.add( new app.Book( formData ) );
   }
 });
