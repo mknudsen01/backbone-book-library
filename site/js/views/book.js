1847a1970c0703de14d5f9d274e44507dc3ffafb
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-21 12:28:01 -0700

    remove extra parenthsis

--- site/js/views/book.js
+++ site/js/views/book.js
@@ -3,7 +3,7 @@ var app = app || {};
 app.BookView = Backbone.View.extend({
   tagName: 'div',
   className: 'bookContainer',
-  template: _.template( $( '#bookTemplate' ).html() ) ),
+  template: _.template( $( '#bookTemplate' ).html() ),
 
   render: function() {
     this.$el.html( this.template( this.model.toJSON() ) );
