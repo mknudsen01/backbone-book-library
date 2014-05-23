4ca1aa3de925ca5880666e9c78a32abb34c53dd7
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-23 16:45:19 -0700

    Search for single book
      * allows search for particular book with its mongo id

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ app.get('/api/books', function( request, response ){
     }
   });
 });
+app.get('/api/books/:id', function( request, response ) {
+  return BookModel.findById( request.params.id, function( err, book ) {
+    if( !err ) {
+      return response.send( book );
+    } else {
+      return console.log( err );
+    }
+  });
+});
 
 app.post( '/api/books', function( request, response ) {
   var book = new BookModel({
