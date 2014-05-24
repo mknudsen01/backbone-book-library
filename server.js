f4aeb3c549a055a29d95c4fa3f6d45d057220989
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-23 17:37:56 -0700

    Add delete route for individual books
      * find book by id
      * remove it from the database

--- server.js
+++ server.js
@@ -85,3 +85,17 @@ app.put( '/api/books/:id', function( request, response ) {
     });
   });
 });
+
+app.delete( '/api/books/:id', function( request, response ) {
+  console.log( 'Deleting bood with id: ' + request.params.id );
+  return BookModel.findById( request.params.id, function( err, book ) {
+    return book.remove( function( err ) {
+      if( !err ) {
+        console.log( 'Book removed' );
+        return response.send( '' );
+      } else {
+        console.log( err );
+      }
+    });
+  });
+});
